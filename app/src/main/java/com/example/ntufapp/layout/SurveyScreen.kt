23c3b67618e9da5b058e8c083744a8e8249916e9
Leo Hsu <leodream251324@gmail.com>
2024-05-28 12:21:09 +0800

    handle the state to ReSurveyScreen

--- app/src/main/java/com/example/ntufapp/layout/SurveyScreen.kt
+++ app/src/main/java/com/example/ntufapp/layout/SurveyScreen.kt
@@ -5,11 +5,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.ntufapp.data.ntufappInfo.Companion.defaultTreeNum
 import com.example.ntufapp.model.PlotData
 import com.example.ntufapp.ui.widget.SurveyView
+import com.example.ntufapp.utils.showMessage
 
 //ref: https://www.youtube.com/watch?v=8XJfLaAOxD0&ab_channel=AndroidDevelopers
 // live-edit for compose
@@ -25,11 +27,7 @@ fun ReSurveyScreen(
     val newPlotDataState = remember { mutableStateOf(newPlotData) }
     newPlotDataState.value.setToday()
     // Store all the tree numbers in ReSurvey Layout
-    val treeNumList = mutableListOf<String>()
-    for (i in 0 until newPlotData.PlotTrees.size) {
-        treeNumList.add((i + 1).toString())
-    }
-    val totalTreesNumList = remember { mutableStateListOf(*treeNumList.toTypedArray()) }
+    val totalTreesNumList = remember { mutableStateListOf(*List(newPlotData.PlotTrees.size){ (it+1).toString() }.toTypedArray()) }
     SurveyView(totalTreesNumList = totalTreesNumList, newPlotData = newPlotDataState.value, onNextButtonClick = onNextButtonClick)
 }
 
@@ -38,25 +36,20 @@ fun NewSurveyScreen(
     newPlotData: PlotData,
     onNextButtonClick: () -> Unit
 ) {
+    val context = LocalContext.current
 //    DisableBackButtonHandler(from = "NewSurveyScreen")
     BackHandler(enabled = true) {}
 
     // Save the final return data
     val newPlotDataState = remember { mutableStateOf(newPlotData) }
     newPlotDataState.value.setToday()
-    // // Store all the tree numbers in ReSurvey Layout
-    val treeNumList = mutableListOf<String>()
-    if (newPlotDataState.value.PlotTrees.size == 0) {
+    // Init the tree number if there is no tree data in the plot
+    if (newPlotDataState.value.PlotTrees.isEmpty()) {
+        showMessage(context,"There are no tree data in the plot, use default tree number $defaultTreeNum.")
         newPlotDataState.value.initPlotTrees(defaultTreeNum)
-        for (i in 0 until defaultTreeNum) {
-            treeNumList.add((i + 1).toString())
-        }
-    } else {
-        for (i in 0 until newPlotDataState.value.PlotTrees.size) {
-            treeNumList.add((i + 1).toString())
-        }
     }
-    val totalTreesNumList = remember { mutableStateListOf(*treeNumList.toTypedArray()) }
+    // Store all the tree numbers in ReSurvey Layout
+    val totalTreesNumList = remember { mutableStateListOf(*List(newPlotData.PlotTrees.size){ (it+1).toString() }.toTypedArray()) }
     SurveyView(totalTreesNumList = totalTreesNumList, newPlotData = newPlotDataState.value, onNextButtonClick = onNextButtonClick)
 }
 
